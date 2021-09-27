03d662f642584b60a7a01ce9b9c7bfd16eb2b6a5
npelvet <75808786+npelvet@users.noreply.github.com>
2021-09-27 16:40:44 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Les canelés d\'isa',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Site de commande de canelés a Toulouse.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
